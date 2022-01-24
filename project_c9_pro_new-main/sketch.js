117ae60beb09f9bfd1f4299e6e4003d6457b9811
lavinia105 <98048929+lavinia105@users.noreply.github.com>
2022-01-24 15:52:14 -0300

    Add files via upload

---
+++ project_c9_pro_new-main/sketch.js
@@ -0,0 +1,65 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+  
+
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  
+  if (keyIsDown(RIGHT_ARROW))
+  {
+    box.velocityX = box.velocityX =2; 
+  }
+
+  if (keyIsDown(LEFT_ARROW))
+  {
+    box.velocityX =box.velocityX = -2;
+  }
+
+  if (keyIsDown(UP_ARROW))
+  {
+    box.velocityX =box.velocityY = -2;
+  }
+
+  if (keyIsDown(DOWN_ARROW))
+  {
+    box.velocityX =box.velocityY = 2
+
+  }
+
+
+
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+  if (keyIsDown(RIGHT_ARROW))
+  {
+      background("red");
+
+  }
+  drawSprites();  
+  
+}
+
